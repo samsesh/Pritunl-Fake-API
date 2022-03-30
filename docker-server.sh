41d76e9366640b606e1ae564efb240b09a39751b
127.0.0.1 <admin@samsesh.net>
2022-03-30 13:38:16 +0430

    Update docker-server.sh

--- docker-server.sh
+++ docker-server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 docker build . --file Dockerfile --tag fakeapipritunl:v1
+docker rm -f FakeApiPritunl
 docker run -d --restart unless-stopped --hostname fakeapi -p 9700:80 --name FakeApiPritunl fakeapipritunl:v1
